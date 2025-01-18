916f257cbd07e6ed4a6b4b2dbe3cfac7c1d1e9aa
XiaoLLiu <lyq10184@outlook.com>
2025-01-18 20:41:58 +0800

    写完用户写数的部分了hhh（想玩ta6

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+use std::io;
+//引用的库，std是标准库，io应该是个类？
+
+fn main() {
+    println!("猜数小游戏hhh");  //就是个提示
+
+    println!("你就猜个数，ok？");
+
+    //let mut yee=1 let声明，mut规定变量可变，未声明及默认变量是不可变的（
+    //let ohh=yee
+    let mut guess = String ::new();  //声明guess变量，string是字符串，加个new是新建
+
+    io ::stdin().read_line(&mut guess).expect("WARNING ERROR 1");  //引用io库，stdin为函数，使用readline和expect方法
+
+    println!("给我个数：{}",guess);  //就是个提示{}会自动替换为后面的所写的变量
+
+
+
+
+
+}

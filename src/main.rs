9640fba1674d6ebead6bb34874beb1d5d8f6281b
XiaoLLiu <lyq10184@outlook.com>
2025-01-18 20:47:12 +0800

    乐，先输入后显示啊？（

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,13 @@ use std::io;
 fn main() {
     println!("猜数小游戏hhh");  //就是个提示
 
-    println!("你就猜个数，ok？");
+    println!("你就先给个数，ok？");
 
     let mut guess = String ::new();  //声明guess变量，string是字符串，加个new是新建
 
     io ::stdin().read_line(&mut guess).expect("WARNING ERROR 1");  //引用io库，stdin为函数，使用readline和expect方法
 
-    println!("给我个数：{}",guess);  //就是个提示{}会自动替换为后面的所写的变量
+    println!("给我的数：{}",guess);  //就是个提示{}会自动替换为后面的所写的变量
 
 
 

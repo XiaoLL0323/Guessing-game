431666805f4c1f21baf17176f3cf493d570d75e0
XiaoLLiu <lyq10184@outlook.com>
2025-01-19 19:57:35 +0800

    安个rand，加个不变的变量用作随机数

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
 use std::io;
+use rand::Rng;//api
 //引用的库，std是标准库，io应该是个类？
 
 fn main() {
     println!("猜数小游戏hhh");  //就是个提示
+
+    let secret_number = rand::thread_rng().gen_range(1..114);
+
+    println!("妙妙数字就是：{}",secret_number);
 
     println!("你就先给个数，ok？");
 

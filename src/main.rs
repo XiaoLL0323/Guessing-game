ecbcf606f207fb742acbad5f758d4e64b0926b07
XiaoLLiu <lyq10184@outlook.com>
2025-01-19 19:57:35 +0800

    完结散花！

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+
 use std::io;
 use rand::Rng;//api
 use std::cmp ::Ordering;
@@ -5,25 +6,33 @@ use std::cmp ::Ordering;
 
 fn main() {
     println!("猜数小游戏hhh");  //就是个提示
-    
-    println!("你就先给个数，ok？");
-
-    let mut guess = String ::new();  //声明guess变量，string是字符串，加个new是新建
-
-    io ::stdin().read_line(&mut guess).expect("WARNING ERROR 1");  //引用io库，stdin为函数，使用readline和expect方法
-
-    println!("给我的数：{}",guess);  //就是个提示{}会自动替换为后面的所写的变量
-
     let secret_number = rand::thread_rng().gen_range(1..114);
-    println!("妙妙数字就是：{}",secret_number);
-
-    let guess: u32 = guess.trim().parse().expect("你反骨啊，我tm让你输个数"); //使guess类型转化为u32以便和同为u32的数字比较
     
-    match guess.cmp(&secret_number) { //以下是属于Ordering的arm（分支），如果匹配到某个分支就会执行管道符后面的代码
+    loop { //loop循环
+        println!("你就给个数，ok？");
+        let mut guess = String ::new();  //声明guess变量，string是字符串，加个new是新建
+        io ::stdin().read_line(&mut guess).expect("WARNING ERROR 1");  //引用io库，stdin为函数，使用readline和expect方法    
         
+        println!("给我的数：{}",guess);  //就是个提示{}会自动替换为后面的所写的变量
+
+        let guess: u32 =  match guess.trim().parse() {
+           Ok(num) => num,
+           Err(_)=> continue
+        };//解决用户输入的不是数字的情况下，是和不是的比较
+    
+        match guess.cmp(&secret_number) { //以下是属于Ordering的arm（分支），如果匹配到某个分支就会执行管道符后面的代码
         Ordering::Less => println!("nonononono,to small!"),
         Ordering::Greater => println!("nonononono,to big!"), 
-        Ordering::Equal => println!("你挺nb啊,一下就猜对了？！"),
+        Ordering::Equal => {
+            println!("nb啊");
+            break;
+        }
+       }
     }
+
+    
+    
+
+   
 
 }
